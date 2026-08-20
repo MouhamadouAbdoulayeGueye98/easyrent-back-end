ce572cd7c32ba8f15cf703e55f665217122848af
Sala1225 <salalearn12@gmail.com>
2026-08-20 10:24:22 +0000

    fix: correction des roles utilisateur

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -29,7 +29,7 @@ export class AuthService {
   // Conversion du rôle mobile vers l'Enum Prisma
   let userRole: Role = Role.CLIENT;
   if (data.role === 'publisher' || data.role === 'ANNONCEUR') {
-    userRole = Role.ANNON CEUR;
+    userRole = Role.ANNONCEUR;
   }
 
   const user = await this.prisma.user.create({
